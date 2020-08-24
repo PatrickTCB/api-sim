2af12a07e842202645aac7bdfe26d0ae7425c5ff
Patrick <ptcb@phn1.net>
2020-08-24 12:05:44 +0200

    Added some formatted JSON with all the URL data

--- app/index.php
+++ app/index.php
@@ -32,8 +32,15 @@
               <h1>API Tester</h1>
               <span class="subheading">
                 <?php 
-                  $actual_link = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+                  $actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
                   echo $actual_link;
+                  function base64url_encode($data) {
+                    return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+                  }
+                  
+                  function base64url_decode($data) {
+                    return base64_decode(str_pad(strtr($data, '-_', '+/'), strlen($data) % 4, '=', STR_PAD_RIGHT));
+                  }
                 ?>
               </span>
             </div>
@@ -59,20 +66,45 @@
           <p>GET Parameters Received:<br />
             <ul>
             <?php
+              $getparams = FALSE;
+              $getparamsjson = "";
+              $getparamsjsonpretty = "";
               foreach($_GET as $key=>$value){
+                $getparams = TRUE;
                 echo "<li>", $key, ' = ', $value, "</li>";
+                $getparamsjson = $getparamsjson . "{\"name\":\"" . $key . "\",\"values\": [\"" . $value . "\"]},";
+                $getparamsjsonpretty = $getparamsjsonpretty . "\t\t{\n\t\t\t\"name\":\"" . $key . "\",\n\t\t\t\"values\": [\n\t\t\t\t\"" . $value . "\"\n\t\t\t]\n\t\t},\n";
+              }
+              echo "</ul>";
+              if ($getparams == TRUE){
+                $scenario = "{\"url\": \"" . $actual_link . "\",\"method\": \"GET\",\"parameters\": [" . rtrim($getparamsjson, ",") . "]}";
+                $prettyscenario = "{\n\t\"url\": \"" . $actual_link . "\",\n\t\"method\": \"GET\",\n\t\"parameters\": [\n" . rtrim($getparamsjsonpretty, ",") . "\n\t]\n}";
+                echo "<p>JSON representation of URL data<br /><pre class=\".code-card\">", $prettyscenario, "</pre></p>";
+                echo "<p>Base64 URL Safe encoding if this scenario data<br /><span>", base64url_encode($scenario), "</span></p>";
               }
             ?>
-            </ul>
           </p>
           <p>POST Parameters Received:<br />
             <ul>
             <?php
+              $postparams = FALSE;
+              $postparamsjson = "";
+              $postparamsjsonpretty = "";
               foreach($_POST as $key=>$value){
+                $postparams = TRUE;
                 echo "<li>", $key, ' = ', $value, "</li>";
+                $postparamsjson = $postparamsjson . "{\"name\":\"" . $key . "\",\"values\": [\"" . $value . "\"]},";
+                $postparamsjsonpretty = $postparamsjsonpretty . "\t\t{\n\t\t\t\"name\":\"" . $key . "\",\n\t\t\t\"values\": [\n\t\t\t\t\"" . $value . "\"\n\t\t\t]\n\t\t},\n";
+              }
+              if ($postparams == TRUE){
+                $scenario = "{\"url\": \"" . $actual_link . "\",\"method\": \"GET\",\"parameters\": [" . rtrim($postparamsjson, ",") . "]}";
+                $prettyscenario = "{\n\t\"url\": \"" . $actual_link . "\",\n\t\"method\": \"GET\",\n\t\"parameters\": [\n" . rtrim($postparamsjsonpretty, ",") . "\n\t]\n}";
+                echo "<p>JSON representation of URL data<br /><pre class=\".code-card\">", $prettyscenario, "</pre></p>";
+                echo "<p>Base64 URL Safe encoding if this scenario data<br /><span>", base64url_encode($scenario), "</span></p>";
               }
             ?>
             </ul>
+            
           </p>
         </div>
       </div>
